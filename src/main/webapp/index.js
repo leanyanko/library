1b82b7fa3abd858585391ae44df580889db3f4ad
Anna Leonenko <mskozlovskaya@gmail.com>
2016-11-15 15:17:40 +0100

    validation

---
+++ src/main/webapp/index.js
@@ -0,0 +1,150 @@
+var url = 'http://localhost:8081/library/rest/books/';
+$(document).ready(
+    $( function() {
+        $( "#dateField" ).datepicker();
+        $.getJSON(url, onBookListSuccess);
+    } )
+);
+
+function validateForNull(value, containerId, msg) {
+    var alert = $('#' + containerId + ' .alert');
+    alert.text(msg)
+    if (value == '') {
+        $('#submit').onclick='validateAll()';
+        alert.removeClass('no-display');
+        return false;
+    }
+    else {
+        alert.addClass('no-display');
+        $('#submit').onclick = 'validateAll()';
+        return true;
+    }
+
+}
+
+function validate(value, containerId, type) {
+    if(!  validateForNull(value, containerId, 'Field must not be empty'))
+        return false;
+
+    switch (type) {
+        case 'shortString':
+          //  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            var re = [^0-9]*;
+            if (!re.test(value)) {
+                $('#submit').onclick='validateAll()';
+                alert.removeClass('no-display');
+                $('#' + containerId + ' .alert').text('Must not contain numbers');
+                return false;
+            }
+            break;
+        case 'date' :
+
+            break;
+    }
+    return true;
+
+}
+
+function validateAll() {
+    var name = document.getElementById('nameField').value;
+
+    var author = document.getElementById('authorField').value;
+
+    var rawDate = document.getElementById('dateField').value;
+
+
+    if (!validate(document.getElementById('nameField').value, 'registerFormBookName', 'shortString')
+        | !validate(author, 'registerFormBookAuthor', 'shortString')
+        | !validate(rawDate, 'registerFormBookDate', 'date'))
+        $('#submit').onclick='validateAll()';
+    else {
+        var date = (new Date(rawDate)).toISOString();
+
+        var book = {};
+        book['name'] = name;
+        book['author'] = author;
+        book['date'] = date;
+
+        register(book);
+    }
+}
+
+function register(book) {
+
+    $.ajax({
+            url: url,
+            type: 'POST',
+            success: function () {
+                console.log('success: Post');
+                $.getJSON(url, onBookListSuccess);
+                document.getElementById('nameField').value='';
+                document.getElementById('authorField').value='';
+            //    document.getElementById('datepicker').value = null;
+            },
+            data: JSON.stringify(book),
+            contentType: 'application/json; charset=utf-8'
+        }
+    )
+}
+
+function getById(i) {
+
+}
+
+function deleteB(i) {
+    var urlToDel = 'http://localhost:8081/library/rest/books/' + i;
+    $.ajax({
+        url: urlToDel,
+        type: 'DELETE',
+        success: function() {
+            console.log('success: Delete');
+            $.getJSON('http://localhost:8081/library/rest/books/', onBookListSuccess);
+        }
+    });
+}
+
+function onBookListSuccess(bookList) {
+    var listDiv = $('#listBooksNames');
+    listDiv.empty();
+
+    var listAu = $('#listBooksAuthor');
+    listAu.empty();
+
+    var listDates = $('#listBooksDate');
+    listDates.empty();
+
+    var liDel = $('#listBooksDelete');
+    liDel.empty();
+
+    var listAll = $('#attach');
+    listAll.empty();
+
+    bookList.forEach(function (book, i) {
+            var bookName = document.getElementById('toClone').cloneNode(false);
+            bookName.id = book.id + book.name;
+            bookName.append(book.name);
+            listAll.append(bookName);
+
+            var bookAuthor = document.getElementById('toClone').cloneNode(false);
+            bookAuthor.id = book.id + book.author;
+            bookAuthor.append(book.author);
+            listAll.append(bookAuthor);
+
+            var bookDate = document.getElementById('toClone').cloneNode(false);
+            var d = new Date(book.date).toLocaleDateString()
+            bookDate.id = book.id + d;
+            bookDate.append(d);
+            listAll.append(bookDate);
+
+            var bookDelete = document.getElementById('toClone').cloneNode(false);
+            bookDelete.id = book.id + 'delete';
+            var del = "<button class=\"delete\" onclick=\"deleteB("+book.id+")\">Delete("+book.id+")</button>";
+            // bookDelete.append(del);
+            bookDelete.innerHTML = del;
+            listAll.append(bookDelete);
+        }
+    );
+
+}
+
+
